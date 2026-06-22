c727bb48cf56b03fb0eed70e12fd6cbfbf4e49a9
Lio <liocicolecchia@gmail.com>
2026-06-22 16:55:42 +0200

    making it possible to close the modal

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -15,6 +15,10 @@ function Cart() {
     0
   );
 
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
   return (
     <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
@@ -27,8 +31,10 @@ function Cart() {
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly>Close</Button>
-        <Button>Go to Checkout</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button onClick={handleCloseCart}>Go to Checkout</Button>
       </p>
     </Modal>
   );
