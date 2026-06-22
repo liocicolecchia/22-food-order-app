31cf9a4349c4c4ff7f62f5a457172d660e0e70a7
Lio <liocicolecchia@gmail.com>
2026-06-22 13:39:48 +0200

    adding MealItem to Meals and fixing image import issue

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+import MealItem from "./MealItem";
+
 function Meals() {
   const [loadedMeals, setLoadedMeals] = useState([]);
 
@@ -21,7 +23,7 @@ function Meals() {
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-        <li key={meal.id}>{meal.name}</li>
+        <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
   );
